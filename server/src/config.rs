8f1e4917a789f5c2a18999f808e4fabedc280597
NomenConservandum <hramov.sanya2018@yandex.ru>
2025-11-21 16:26:10 +0400

    Add: possible db connection basics

---
+++ server/src/config.rs
@@ -0,0 +1,22 @@
+use sqlx::postgres::PgPoolOptions;
+use std::env;
+
+pub struct Config {
+    pub database_url: String,
+}
+
+impl Config {
+    pub fn from_env() -> Self {
+        Self {
+            database_url: env::var("DATABASE_URL")
+                .unwrap_or("postgres://postgres:postgres@localhost:5432/interestograph".into()),
+        }
+    }
+}
+
+pub async fn get_db_pool(database_url: &str) -> sqlx::PgPool {
+    PgPoolOptions::new()
+        .max_connections(5) // Set maximum number of connections
+        .connect(&database_url)
+        .await.unwrap()
+}
